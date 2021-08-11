d4b534a3e036413934d63064530071294e8dfc92
Cheng Liu <liucheng.tech@outlook.com>
2021-08-11 19:30:27 +0800

    :sparkles: error handling

--- src/cli.ts
+++ src/cli.ts
@@ -29,58 +29,68 @@ program
     if (extension) {
       config.extension = extension;
     }
-    const output = await scanDeps(config);
-    if (json) {
-      console.log(JSON.stringify(output, null, 2));
-    } else {
-      const { missingDependencies, unusedDependencies, detectedDependencies } =
-        output;
+    try {
+      const output = await scanDeps(config);
+      if (json) {
+        console.log(JSON.stringify(output, null, 2));
+      } else {
+        const {
+          missingDependencies,
+          unusedDependencies,
+          detectedDependencies,
+        } = output;
 
-      // detected deps
-      if (all) {
-        if (detectedDependencies.length !== 0) {
-          console.log(colors.yellow("Detected dependencies"));
-          console.log(" - these seem to be imported by this project:");
-          for (const packageName of detectedDependencies) {
-            console.log("  " + packageName);
+        // detected deps
+        if (all) {
+          if (detectedDependencies.length !== 0) {
+            console.log(colors.yellow("Detected dependencies"));
+            console.log(" - these seem to be imported by this project:");
+            for (const packageName of detectedDependencies) {
+              console.log("  " + packageName);
+            }
+          } else {
+            console.log(
+              "This project does not seem to import any NPM packages."
+            );
           }
-        } else {
-          console.log("This project does not seem to import any NPM packages.");
-        }
-        console.log();
-      }
-
-      let wroteAnything: boolean = false;
-
-      if (missingDependencies.length > 0) {
-        console.log(colors.yellow("Possible phantom dependencies"));
-        console.log(
-          " - these seem to be imported but aren't listed in package.json:"
-        );
-        for (const packageName of missingDependencies) {
-          console.log("  " + packageName);
-        }
-        wroteAnything = true;
-      }
-
-      if (unusedDependencies.length > 0) {
-        if (wroteAnything) {
           console.log();
         }
-        console.log(colors.yellow("Possible unused dependencies"));
-        console.log(
-          " - these are listed in package.json but don't seem to be imported:"
-        );
-        for (const packageName of unusedDependencies) {
-          console.log("  " + packageName);
-        }
-        wroteAnything = true;
-      }
 
-      if (!wroteAnything) {
-        console.log(colors.green("Everything looks good."));
-        console.log("  No missing or unused dependencies were found.");
+        let wroteAnything: boolean = false;
+
+        if (missingDependencies.length > 0) {
+          console.log(colors.yellow("Possible phantom dependencies"));
+          console.log(
+            " - these seem to be imported but aren't listed in package.json:"
+          );
+          for (const packageName of missingDependencies) {
+            console.log("  " + packageName);
+          }
+          wroteAnything = true;
+        }
+
+        if (unusedDependencies.length > 0) {
+          if (wroteAnything) {
+            console.log();
+          }
+          console.log(colors.yellow("Possible unused dependencies"));
+          console.log(
+            " - these are listed in package.json but don't seem to be imported:"
+          );
+          for (const packageName of unusedDependencies) {
+            console.log("  " + packageName);
+          }
+          wroteAnything = true;
+        }
+
+        if (!wroteAnything) {
+          console.log(colors.green("Everything looks good."));
+          console.log("  No missing or unused dependencies were found.");
+        }
       }
+    } catch (e) {
+      console.log(colors.red(e.message));
+      process.exit(1);
     }
   });
 
